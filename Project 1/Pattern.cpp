6896e7dc7b5285f8b5ab727c0ff58efa08ddc725
luhenaabebe <125544318+luhenaabebe@users.noreply.github.com>
2023-04-17 12:19:02 +0300

    Update Pattern.cpp

--- Project 1/Pattern.cpp
+++ Project 1/Pattern.cpp
@@ -1,1 +1,38 @@
+#include<iostream>
 
+using namespace std;
+
+
+
+int main() {
+    int n;
+    cout << "Enter a positive integer n: ";
+    cin >> n;
+    if(n<=0){
+     return 0;}
+    for (int i = n; i >= 0; i--) {
+        for (int j = 0; j < n - i; j++) {
+            cout << "  ";
+        }
+        for (int j = 0; j <= i; j++) {
+            cout << j << " ";
+        }
+        for (int j = i - 1; j >= 0; j--) {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j < n - i; j++) {
+            cout << "  ";
+        }
+        for (int j = 0; j <= i; j++) {
+            cout << j << " ";
+        }
+        for (int j = i - 1; j >= 0; j--) {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
