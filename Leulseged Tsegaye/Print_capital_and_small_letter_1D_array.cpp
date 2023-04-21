df272c8ebfef9ef1a292baace64085ec15a835ab
leulsegedtsegaye <124517236+leulsegedtsegaye@users.noreply.github.com>
2023-04-21 09:52:52 +0300

    Create Print_capital_and_small_letter_1D_array.cpp

---
+++ Leulseged Tsegaye/Print_capital_and_small_letter_1D_array.cpp
@@ -0,0 +1,13 @@
+//1D array print capital and small letter
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+	char cap[6]={65,66,67,68,69,70};
+	char small[6]={97,98,99,100,101,101};
+	for(int i=0;i<6;i=i+1){
+		cout<<cap[i]<<small[i]<<" ";
+		}
+		exit(0);
+return 0;
+}
