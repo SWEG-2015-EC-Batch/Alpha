12e878d3f489098fe997a2d777aadbda33a5c43f
Finotmah <124761678+Finotmah@users.noreply.github.com>
2023-06-09 10:03:37 -0700

    Add files via upload

---
+++ FOP2/Mahider Yizengaw/Modular Programming/welcome.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+ void greeting();
+
+int main(){
+ greeting();
+ return 0;   
+}
+void greeting(){
+     string name;
+    cout << "please enter your name" <<endl;
+    getline(cin,name);
+    cout << "hello " << name << endl;
+    cout << "welcome to m.p!!";
+}
