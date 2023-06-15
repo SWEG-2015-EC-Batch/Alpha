5c84a9edce690d0a3352626d98029795b7afa7ce
Mahider Jemere <124486265+Mahi-gitlex@users.noreply.github.com>
2023-06-15 18:44:53 +0300

    Update Fibonnaci_default_arg.cpp

--- FoP-II/Mahider Jemere/Class Activity/Modular Programming/Fibonnaci_default_arg.cpp
+++ FoP-II/Mahider Jemere/Class Activity/Modular Programming/Fibonnaci_default_arg.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+using namespace std;
+//This program shows the use of default argument in finding a fibonnaci series
+int fibonacci(int=5);
+int main()
+{
+
+
+    fibonacci(8);
+    cout<<endl;
+    fibonacci();
+
+    return 0;
+}
+
+int fibonacci(int num){
+
+    int a=0;
+    int b=1;
+    int d=0;
+
+    for (int i=0;i<num;i++){
+            d=a+b;
+            b=a;
+            a=d;
+            cout<<d<<endl;
+    }
+    return 0;
+}
 
