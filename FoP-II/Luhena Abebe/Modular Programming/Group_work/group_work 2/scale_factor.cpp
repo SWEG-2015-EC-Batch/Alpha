9ca96e83b387e8bd91a319d02ab249df8a689e5e
luhenaabebe <125544318+luhenaabebe@users.noreply.github.com>
2023-06-18 17:48:45 +0300

    Create scale_factor.cpp

---
+++ FoP-II/Luhena Abebe/Modular Programming/Group_work/group_work 2/scale_factor.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cassert>
+using namespace std;
+void scale(double scale_factor,double &fnum,double &snum);
+
+int main()
+{   double scale_f,fnum,snum;
+    cout<<"This code is used to scale the input given by the user "<<endl;
+    cout<<"scale factor "<<endl;
+    cin>>scale_f;
+    cout<<"Please enter first value"<<endl;
+    cin>>fnum;
+    cout<<"Please enter second value"<<endl;
+    cin>>snum;
+    cout<<"the values before scaling: "<<endl;
+    cout<<fnum<<" " <<snum<<endl;
+    cout<<"the values after scaling: "<<endl;
+    scale(scale_f,fnum,snum);
+    cout<<fnum<<"  "<<snum<<endl;
+    return 0;
+}
+
+void scale(double scale_factor,double &fnum,double &snum){
+assert (scale_factor!=0);
+fnum=scale_factor*fnum;
+snum=scale_factor*snum;
+}
