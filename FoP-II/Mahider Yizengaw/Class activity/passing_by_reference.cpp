36da912f3a775471d4974e5084ab4ec46539a05e
Finotmah <124761678+Finotmah@users.noreply.github.com>
2023-06-18 07:31:25 -0700

    Update passing_by_reference.cpp

--- FoP-II/Mahider Yizengaw/Class activity/passing_by_reference.cpp
+++ FoP-II/Mahider Yizengaw/Class activity/passing_by_reference.cpp
@@ -1,1 +1,21 @@
+#include<iostream>
 
+using namespace std;
+
+void calcquotient(double n1,double n2, double &answer);
+
+int main(){
+    double num1 = 8;
+    double num2 = 2;
+    double quotient;
+
+    calcquotient(num1,num2,quotient);
+
+    cout << "the quotient is " << quotient;
+
+    return 0;
+}
+
+ void calcquotient(double n1,double n2,double &answer){
+   answer = n1/n2;     
+    }
