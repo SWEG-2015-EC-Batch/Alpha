fd09f0d1591a58fd746b9af0f5d7216c81a72dda
luhenaabebe <125544318+luhenaabebe@users.noreply.github.com>
2023-06-24 00:36:29 +0300

    Update welcome.cpp

--- FoP-II/Luhena Abebe/Modular Programming/class activities/activity 1/welcome.cpp
+++ FoP-II/Luhena Abebe/Modular Programming/class activities/activity 1/welcome.cpp
@@ -1,1 +1,19 @@
+//This programme written for the purpose of function declaration and definition.
+#include <iostream>
+#include <string.h>
+using namespace std;
+void welcome();
+
+int main()
+{
+    welcome();
+    return 0;
+}
+
+void welcome(){
+string name;
+cout<<"please enter your name"<<endl;
+getline(cin,name);
+cout<<"hello "<<name<<" welcome to modular programming";
+}
 
