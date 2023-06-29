a80d231a1d6ea128610d1493fa2ae8da9c11c502
Mahider Jemere <124486265+Mahi-gitlex@users.noreply.github.com>
2023-06-29 22:28:35 +0300

    Update Student_inf.cpp

--- FoP-II/Mahider Jemere/Class Activity/Modular Programming/Activity 4/Student_inf.cpp
+++ FoP-II/Mahider Jemere/Class Activity/Modular Programming/Activity 4/Student_inf.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+struct student{
+    string id;
+    string name;
+    float testMark;
+    float finalMark;
+    float sumMark;
+};
+int main()
+{
+    student stud;
+    cout<<"PLease enter the name of the student: ";
+    getline(cin,stud.name);
+    cout<<"Please enter the ID of the student: ";
+    getline(cin,stud.id);
+    cout<<"Please enter the test mark of the student: ";
+    cin>>stud.testMark;
+    cout<<"Please enter the final mark of the student: ";
+    cin>>stud.finalMark;
+
+    stud.sumMark=stud.finalMark+stud.testMark;
+
+    cout<<"Name: "<<stud.name<<endl;
+    cout<<"ID: "<<stud.id<<endl;
+    cout<<"Test mark: "<<stud.testMark<<"\nFinal mark: "<<stud.finalMark<<"\nSum: "<<stud.sumMark;
+
+    return 0;
+}
 
