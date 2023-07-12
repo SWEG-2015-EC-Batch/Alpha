196914e10d66672333ce7b626f111f3a5435a68e
Finotmah <124761678+Finotmah@users.noreply.github.com>
2023-07-11 21:50:48 -0700

    Create Sheet5Ex5.cpp

---
+++ FoP-II/Mahider Yizengaw/Modular Programming/Homworks/file management/Q8/Sheet5Ex5.cpp
@@ -0,0 +1,6 @@
+C++ is a high-level, general-purpose programming language created by Danish computer scientist Bjarne Stroustrup.
+It is an extension of the C programming language. 
+  C++ is an object-oriented programming language which gives a clear structure to programs and allows code to be reused, lowering development costs.
+C++ is portable and can be used to develop applications that can be adapted to multiple platforms.
+  It gives programmers a high level of control over system resources and memory2. C++ is one of the world’s most popular programming languages. 
+  It can be found in today’s operating systems, Graphical User Interfaces, and embedded systems
